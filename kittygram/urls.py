7b8acb1dd179d824c9aba33a3c44d7c8f133ab1a
Artur Muratov <a.muratov86@yandex.ru>
2023-04-23 21:59:44 +0500

    fork rep

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,22 @@
 from django.urls import include, path
 
-from cats.views import cat_list
+#from cats.views import cat_list
+#from cats.views import APICat
+from cats.views import CatList, CatDetail
+
+from rest_framework.routers import SimpleRouter
+from cats.views import CatViewSet
+
+router = SimpleRouter()
+
+router.register('cats', CatViewSet) 
 
 urlpatterns = [
-   path('cats/', cat_list),
+   #path('cats/', cat_list),
+   #path('cats/', APICat.as_view()),
+   #path('cats/', CatList.as_view()),
+   #path('cats/<int:pk>/', CatDetail.as_view()),
+   path('', include(router.urls)),
 ]
 
 
